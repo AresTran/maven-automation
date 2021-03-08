bc4ae51f2d90242f2ae25caf4f8f223c23266645
AresTran <thanhan311@gmail.com>
2021-03-08 16:50:48 +0700

    first-lesson 20210308_1700

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+//        String text = "Hello World";
+//        System.out.println(text);
+//        System.out.println("Hello World");
+
+        Student stu = new Student();
+        stu.setFullName("Thanh An");
+        stu.setGender(false);
+//        stu.setBirthday("03-11-1990 00:00:00");
+
+    }
+}
